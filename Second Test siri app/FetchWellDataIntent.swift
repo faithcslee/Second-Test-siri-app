a23f9583ef6af01176ef57259c41e225b0e175b4
faithcslee <faithlee32005@gmail.com>
2024-06-19 11:25:31 -0500

    Added working app intent, entity, query

--- Second Test siri app/FetchWellDataIntent.swift
+++ Second Test siri app/FetchWellDataIntent.swift
@@ -9,6 +9,96 @@ import AppIntents
 
 struct FetchWellDataIntent: AppIntent {
     static var title: LocalizedStringResource = "Fetch Well Data by Intent"
+    static var description = IntentDescription("Fetches the production value for a given well name from a JSON file.")
+
+    @Parameter(title: "Well Name")
+    var wellName: String
+
+    static var parameterSummary: some ParameterSummary {
+        Summary("Fetch value for \(\.$wellName)")
+    }
+    
+    @MainActor
+    func perform() async throws -> some IntentResult & ReturnsValue<String> & ProvidesDialog {
+        // Fetch entities matching the wellName using WellProdQuery
+        let entities = try await WellProd.defaultQuery.entities(matching: wellName)
+        
+        if let wellProd = entities.first {
+            let dialog = IntentDialog("The production value for \(wellName) is \(wellProd.production).")
+            return .result(value: "\(wellProd.production)", dialog: dialog)
+        } else {
+            let dialog = IntentDialog("The production value for \(wellName) was not found.")
+            return .result(value: "", dialog: dialog)
+        }
+    }
+}
+
+
+/*
+import AppIntents
+
+struct FetchWellDataIntent: AppIntent {
+    static var title: LocalizedStringResource = "Fetch Well Data by Intent"
+    static var description = IntentDescription("Fetches the production value for a given well Name from a JSON file.")
+
+    @Parameter(title: "Well Name")
+    var wellName: String
+
+    static var parameterSummary: some ParameterSummary {
+        Summary("Fetch value for \(\.$wellName)")
+    }
+    
+    @MainActor
+    func perform() async throws -> some IntentResult & ReturnsValue<String> & ProvidesDialog {
+        let currentFilePath = URL(fileURLWithPath: #file) //says file doesnt exist
+        // Get the directory of the current script
+        let currentDirectory = currentFilePath.deletingLastPathComponent()
+        // Construct the file URL for the JSON file
+        let fileURL = currentDirectory.appendingPathComponent("sampleProdData.json")
+        
+        // Load the JSON file
+        guard let data = try? Data(contentsOf: fileURL) else {
+            fatalError("Unable to load the JSON file.")
+        }
+                
+        // Decode the JSON data
+        guard let productionDataArray = try? JSONDecoder().decode([[String]].self, from: data) else {
+            fatalError("Unable to decode the JSON data.")
+        }
+        
+        var value:String?
+        for well in productionDataArray{
+            if well[0] == wellName {
+                value = well[2]
+                break
+            }
+            
+        }
+
+        //let dialog = IntentDialog("The production value for \(wellName) is \(value).")
+
+       // return .result(value: "\(value)", dialog: dialog)
+        
+        if let foundValue = value {
+            let dialog = IntentDialog("The production value for \(wellName) is \(foundValue).")
+            return .result(value: foundValue, dialog: dialog)
+        } else {
+            let dialog = IntentDialog("The production value for \(wellName) was not found.")
+            return .result(value: "", dialog: dialog)
+        }
+    }
+}
+*/
+
+
+
+
+
+
+
+/*
+struct FetchWellDataIntent: AppIntent {
+    static var title: LocalizedStringResource = "Fetch Well Data by Intent"
     static var description = IntentDescription("Fetches the production value for a given well Name from a JSON file.")
 
     @Parameter(title: "Well Name")
@@ -59,3 +149,4 @@ struct FetchWellDataIntent: AppIntent {
     }
 }
 
+*/
--- Second Test siri app/LoadData.swift
+++ Second Test siri app/LoadData.swift
@@ -4,6 +4,46 @@
 //
 //  Created by Travis Wadman on 6/14/24.
 //
+
+import Foundation
+
+struct WellData: Identifiable {
+    let id = UUID()
+    let wellName: String
+    let date: String
+    let production: Int
+}
+
+func loadAndParseJSONData() -> [WellData] {
+    guard let url = Bundle.main.url(forResource: "sampleProdData", withExtension: "json") else {
+        print("File not found")
+        return []
+    }
+    
+    do {
+        let data = try Data(contentsOf: url)
+        guard let jsonArray = try JSONSerialization.jsonObject(with: data, options: []) as? [[Any]] else {
+            print("Failed to convert JSON data to array")
+            return []
+        }
+        
+        let wellDataArray = jsonArray.compactMap { entry -> WellData? in
+            if entry.count >= 3,
+               let wellName = entry[0] as? String,
+               let date = entry[1] as? String,
+               let production = entry[2] as? Int {
+                return WellData(wellName: wellName, date: date, production: production)
+            }
+            return nil
+        }
+        
+        return wellDataArray
+    } catch {
+        print("Error reading or parsing the JSON file: \(error)")
+        return []
+    }
+}
+
 /*
 import Foundation
 
