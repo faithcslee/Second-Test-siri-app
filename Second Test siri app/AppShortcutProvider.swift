006d5109756db69b960a401562ed5a9dc2fbfb52
faithcslee <faithlee32005@gmail.com>
2024-06-20 16:17:13 -0500

    Added AppShortcutProvider file

---
+++ Second Test siri app/AppShortcutProvider.swift
@@ -0,0 +1,24 @@
+//
+//  AppShortcutProvider.swift
+//  Second Test siri app
+//
+//  Created by Travis Wadman on 6/19/24.
+//
+
+/*
+import AppIntents
+
+class FetchValueShortcuts: AppShortcutsProvider {
+
+    static var appShortcuts: [AppShortcut] {
+        AppShortcut(intent: FetchWellDataIntent(), phrases: [
+            "Fetch production for \(\.$well) in \(.applicationName)",
+            "Get production for \(\.$well) in \(.applicationName)"
+            ],
+            shortTitle: "Fetch Production",
+            systemImageName: "magnifyingglass.circle"
+        )
+    }
+}
+
+*/
